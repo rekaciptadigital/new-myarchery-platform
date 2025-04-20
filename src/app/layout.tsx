cee25ffa20cc7ad497845a5623947c6267fbe49b
ltmoerdani <ltmoerdani@gmail.com>
2025-04-20 16:33:13 +0700

    feat: add favicon to the application
    Include a favicon to improve the visual identity and user experience of the application by displaying a custom icon in the browser tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "MyArchery",
   description: "Archery Super Platform",
+  icons: {
+    icon: '/icon-myarchery.ico',
+  },
 };
 
 export default function RootLayout({
